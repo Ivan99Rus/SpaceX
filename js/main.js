e1b58d9496412638f1021cbf5e9090fc9683bbe7
Ivan99Rus <ivananapa@mail.ru>
2020-08-05 00:30:35 +0400

    feat

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var instance = new vidbg('.video', {
+  mp4: 'video/world.mp4',
+  webm: 'video/world.webm',
+  poster: 'img/poster.jpg',
+  overlay: false
+});
+
+var rellax = new Rellax('.rocket');
